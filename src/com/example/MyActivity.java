47819bcaca19bd74bf7f9f7bb835f6963145a83c
andik1212 <mulo1212@ukr.net>
2012-10-26 22:21:59 +0300

    lesson1

---
+++ src/com/example/MyActivity.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    public void doSmt(View view){
+        Intent i = new Intent(this, MyActivityTwo.class);
+        startActivity(i);
+    }
+}
---
+++ src/com/example/MyActivityTwo.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivityTwo extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+
+}
